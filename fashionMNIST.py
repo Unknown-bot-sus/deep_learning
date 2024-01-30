3ad8cb355eaf758af2f3c4883329983963282159
Thar Lin Shwe <albent2017@gmail.com>
2024-01-30 11:56:41 +0800

    Testing Dense and Flattern layers

---
+++ fashionMNIST.py
@@ -0,0 +1,23 @@
+import tensorflow as tf 
+import numpy as np 
+from tensorflow.keras import Sequential 
+from tensorflow.keras.layers import Dense, Flatten
+
+data = tf.keras.datasets.fashion_mnist
+
+(trainig_images, training_labels), (test_images, test_labels) = data.load_data()
+
+trainig_images = trainig_images / 255.0
+test_images = test_images / 255.0
+
+model = Sequential([
+    Flatten(input_shape=(28, 28)),
+    Dense(128, activation=tf.nn.relu),
+    Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=['accuracy'])
+
+model.fit(trainig_images, training_labels, epochs=5)
+
+model.evaluate(test_images, test_labels)
